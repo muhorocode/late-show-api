f9824c50435eca6a65101a92deaa9fec846b1118
Elijah Kamanga <elijahkamanga1@gmail.com>
2025-11-23 14:38:16 +0300

    Set up SQLAlchemy, Flask-Migrate, and Flask-RESTful

--- server/app.py
+++ server/app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restful import Api
 
+#initialize flask app
 app=Flask(__name__)
+
+#configure SQLite database
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+#initialize extensions
+db=SQLAlchemy(app)
+migrate=Migrate(app,db)
+api=Api(app)
 
 @app.route('/')
 def home():
